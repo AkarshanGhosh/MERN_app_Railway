5b11acdb941b8ee245a85de4d01cde3a6b0f30de
AkarshanGhosh <ronighosh494@gmail.com>
2024-12-18 23:04:48 +0530

    Update verificationToken.js

--- backend/models/verificationToken.js
+++ backend/models/verificationToken.js
@@ -1,14 +1,13 @@
-const bcrypt = require('bcryptjs/dist/bcrypt');
-const { type } = require('express/lib/response');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Define the schema
 const verificationTokenSchema = new Schema({
-    owner:{
+    owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
     },
     token: {
         type: String,
@@ -16,26 +15,26 @@ const verificationTokenSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        expires: 180,
-        default: Date.now()
-    }
+        expires: 180, // TTL for token expiration (180 seconds)
+        default: Date.now, // Use Date.now without parentheses
+    },
 });
 
-verificationTokenSchema.pre("save", async function (next){
-    if (this.isModified("token")){
+// Pre-save hook for hashing the token
+verificationTokenSchema.pre("save", async function (next) {
+    if (this.isModified("token")) {
         const hash = await bcrypt.hash(this.token, 8);
         this.token = hash;
     }
-
     next();
-    
 });
 
-verificationTokenSchema.mrthod.compareToken = async function (token){
-    const result = await bcrypt.compareSync(token, this.token);
-    return result;
-}
+// Add method to compare tokens
+verificationTokenSchema.methods.compareToken = async function (token) {
+    return await bcrypt.compare(token, this.token); // Use bcrypt.compare (async)
+};
 
-const VerificationToken  = mongoose.model('VerificationToken', verificationTokenSchema);
-//User.createIndexes()
-module.exports = VerificationToken 
+// Create the model
+const VerificationToken = mongoose.model('VerificationToken', verificationTokenSchema);
+
+module.exports = VerificationToken;
